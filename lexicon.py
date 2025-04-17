9a3cc102fbbe5815124779d414c6eabb18f6f672
Jake <jake.gameroff@mail.mcgill.ca>
2025-04-17 19:44:05 -0400

    Added a method to find all (valid) anagrams of a given string, supporting blank tiles too.

--- lexicon.py
+++ lexicon.py
@@ -1,9 +1,11 @@
+from itertools import permutations
 import random
 import json
 
 class Lexicon:
     def __init__(self, dict_name, dict_file):
         self.dict_name = dict_name
+        self.alphabet = [chr(i) for i in range(ord('a'), ord('z') + 1)] + ['?']
 
         with open(f"dictionaries/{dict_file}", "r") as d:
             self.lexicon = set(json.load(d))
@@ -23,13 +25,28 @@ class Lexicon:
     def __iter__(self):
         return iter(self.lexicon)
 
+    def len_k_words(self, k):
+        k_words = [w for w in self if len(w) == k]
+        k_words.sort()
+        return k_words
+
+    def anagrams(self, s):
+        alphabet = [a for a in self.alphabet if a != '?']
+
+        anagrams_no_b = lambda s : set("".join(t) for t in permutations(s)) & self.lexicon
+        anagrams_one_b = lambda s : set.union(*[anagrams_no_b(s.replace('?', a)) for a in alphabet])
+
+        blanks = [i for i in range(len(s)) if s[i] == '?']
+        if blanks == []:
+            return anagrams_no_b(s)
+        elif len(blanks) == 1:
+            return anagrams_one_b(s)
+        else:
+            b = blanks.pop()
+            replace_ith = lambda s, i, r: s[:i] + r + s[i+1:]
+            return set.union(*[anagrams_one_b(replace_ith(s, b, a)) for a in alphabet])
+
 if __name__ == "__main__":
-    l = Lexicon('NWL23', 'nwl23.json')
-    two_letter_words = [w for w in l if len(w) == 2]
-    two_letter_words.sort()
-
-    vowels = ['a', 'e', 'i', 'o', 'u', 'y']
-
-    two_letter_words_no_vowels = [w for w in two_letter_words if len([v for v in vowels if v in w]) == 0]
-
-    print(two_letter_words_no_vowels)
+    l = Lexicon("NWL23", "nwl23.json")
+    s = 'z??'
+    print(l.anagrams(s))
